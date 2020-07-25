0f46747653eedb334ac0803d14ae79f434b06b0b
Daniel Trone <danieljtrone@gmail.com>
2020-07-25 17:36:23 -0500

    reorganize

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import './index.css';
+
+ReactDOM.render(
+  <React.Fragment>
+    <CssBaseline />
+    <App />
+  </React.Fragment>,
+  document.getElementById('root'),
+);
